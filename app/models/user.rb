23ea17e1185832e6ab74351052e0caa8c63826f0
Felipe T Tristao <f.torrestristao@yahoo.com.br>
2016-01-19 16:56:03 -0200

    Finished email activation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,15 @@
 class User < ActiveRecord::Base
-	attr_accessor :remember_token
+	attr_accessor :remember_token, :activation_token, :reset_token
 
-	before_save { self.email = email.downcase }
+	before_save :downcase_email
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 	validates :name, presence:true, length: {maximum: 50}
 	validates :email, presence:true, length: {maximum: 255}, format: {with: VALID_EMAIL_REGEX}, uniqueness: { case_sensitive: false }
 
 	has_secure_password
 	validates :password, length: {minimum: 6}, allow_blank: true
+
+	before_create :create_activation_digest
 
 	# Returns the hash digest of the given string.
   def User.digest(string)
@@ -27,13 +29,49 @@ class User < ActiveRecord::Base
 	end
 
 	#returns true if the given token matches the digest
-	def authenticated?(remember_token)
-		return false if remember_digest.nil?
-		BCrypt::Password.new(remember_digest).is_password?(remember_token)
+	def authenticated?(attribute, token)
+		digest = send("#{attribute}_digest")
+		return false if digest.nil?
+		BCrypt::Password.new(digest).is_password?(token)
 	end
 
 	# forgest a user
 	def forget
 		update_attribute(:remember_digest, nil)
 	end
+
+	def activate
+		update_attribute(:activated, true)
+		update_attribute(:activated_at, Time.zone.now)
+	end
+
+	def send_activation_email
+		UserMailer.account_activation(self).deliver_now
+	end
+
+	# Sets the password reset attributes.
+  def create_reset_digest
+    self.reset_token = User.new_token
+    update_attribute(:reset_digest,  User.digest(reset_token))
+    update_attribute(:reset_sent_at, Time.zone.now)
+  end
+
+	# Sends password reset email.
+  def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now
+  end
+
+	def password_reset_expired?
+		reset_sent_at < 2.hours.ago
+	end
+
+	private
+		def create_activation_digest
+			self.activation_token = User.new_token
+			self.activation_digest = User.digest(activation_token)
+		end
+
+		def downcase_email
+			self.email = email.downcase
+		end
 end
--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -1,6 +1,11 @@
 require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
+
+  def setup
+    ActionMailer::Base.deliveries.clear
+  end
+
   test "unsuccessful signup" do
      get signup_path
 
@@ -13,15 +18,36 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
      assert_template 'users/new'
   end
 
-  test "successful sign up" do
+  test "valid sign up information with account activation" do
     get signup_path
 
     assert_difference 'User.count', 1 do
-      post_via_redirect users_path, user: {name: "Example User",
+      post users_path, user: {name: "Example User",
                                               email: "user@example.com",
                                               password: "password",
                                               password_confirmation: "password"}
     end
+
+    assert_equal 1, ActionMailer::Base.deliveries.size
+    user = assigns(:user)
+    assert_not user.activated?
+
+    #Try to log in before activation
+    log_in_as(user)
+    assert_not is_logged_in?
+
+    #Invalid activation token
+    get edit_account_activation_path("Invalid token")
+    assert_not is_logged_in?
+
+    #Valid token, wrong email
+    get edit_account_activation_path(user.activation_token, email: 'wrong')
+    assert_not is_logged_in?
+
+    #Valid activation token
+    get edit_account_activation_path(user.activation_token, email: user.email)
+    assert user.reload.activated?
+    follow_redirect!
     assert_template 'users/show'
     assert is_logged_in?
   end
